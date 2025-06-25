486f1d43b3cdea6dee54e944b31a02fffe8bac97
Brandon Bwoni <83072016+brandon-bwoni@users.noreply.github.com>
2025-06-25 05:24:07 +0200

    changed the rag implementation into a langgraph tool

--- rag/rag.py
+++ rag/rag.py
@@ -1,16 +1,18 @@
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
 
 load_dotenv()
 
-faq_entries = [
-    "What is your refund policy? You can cancel up to 48 hours before check-in for a full refund.",
-    "How does the loyalty program work? Members earn 1 point per €1 spent.",
-    "Check-in: 3 PM, Check-out: 11 AM.",
-    "Pets allowed under 25kg for a €20 fee.",
-    "Continental breakfast included with all room rates.",
-]
+file_path = "./document/hotel_faq_document.pdf"
 
-emb = OpenAIEmbeddings(model_name="BAAI/bge-m3")
-vectorstore = FAISS.from_texts(faq_entries, emb)
+   
+
+loader = PyPDFLoader(file_path)
+docs = loader.load()
+print(f"Loaded {len(docs)} documents.")
+print(f"Document metadata: {docs[0].metadata}")
+
+emb = OpenAIEmbeddings(model="text-embedding-3-large")
+vectorstore = FAISS.from_documents(docs, emb)
