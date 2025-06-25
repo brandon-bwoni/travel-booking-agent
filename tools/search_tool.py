a1770af2425fa5a63b6f3e54589ce11329403fb9
Brandon Bwoni <83072016+brandon-bwoni@users.noreply.github.com>
2025-06-25 05:27:29 +0200

    added tools: search_travel_info, search_hotels, search_flight

--- tools/search_tool.py
+++
@@ -1,8 +0,0 @@
-from langchain.tools import Tool
-from search.google_search import search_fn
-
-search_tool = Tool(
-    name="search",
-    func=search_fn,
-    description="Fetch up-to-date web results via SerpAPI.",
-)
